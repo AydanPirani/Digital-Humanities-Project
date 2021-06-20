1033174606af7e121d3f94c1ea181fd84677cd1a
Aydan Pirani <aydanpirani@gmail.com>
2021-06-19 22:57:23 -0700

    Added FaceTools init

--- FaceTools.py
+++ FaceTools.py
@@ -3,3 +3,17 @@ import mediapipe as mp
 
 class FaceDetectionTools:
     def __init__(self):
+        self.MP_mesh = mp.solutions.face_mesh
+        self.mesh = self.MP_mesh.FaceMesh()
+
+        self.MP_detection = mp.solutions.face_detection
+        self.detection = self.MP_detection.FaceDetection()
+
+        self.MP_draw = mp.solutions.drawing_utils
+        pass
+
+    def detect_face(self):
+        pass
+
+    def generate_mesh(self):
+        pass
