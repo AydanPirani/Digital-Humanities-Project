e83b9d1934bc420c150969b16e68e7f017182bb8
Aydan Pirani <aydanpirani@gmail.com>
2021-06-19 23:10:54 -0700

    Updated FaceTools methods, added driver program

--- FaceTools.py
+++ FaceTools.py
@@ -1,19 +1,43 @@
 import cv2
 import mediapipe as mp
 
+
 class FaceDetectionTools:
     def __init__(self):
-        self.MP_mesh = mp.solutions.face_mesh
-        self.mesh = self.MP_mesh.FaceMesh()
-
+        self.MP_facemesh = mp.solutions.face_mesh
         self.MP_detection = mp.solutions.face_detection
+
+        self.facemesh = self.MP_facemesh.FaceMesh()
         self.detection = self.MP_detection.FaceDetection()
+
+        self.faces = []
+        self.landmarks = []
 
         self.MP_draw = mp.solutions.drawing_utils
         pass
 
-    def detect_face(self):
-        pass
+    def detect_face(self, img):
+        new_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.detection.process(new_img)
+        faces = results.detections
+        self.faces = []
+        if faces:
+            for face in faces:
+                self.faces.append(face)
+        return
 
-    def generate_mesh(self):
-        pass
+    def generate_mesh(self, img):
+        new_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = self.facemesh.process(new_img)
+        landmarks = results.multi_face_landmarks
+        self.landmarks = []
+        if landmarks:
+            for landmark_list in landmarks:
+                self.landmarks.append(landmark_list)
+        return
+
+    def draw(self, img):
+        for f in self.faces:
+            self.MP_draw.draw_detection(img, f)
+        for l in self.landmarks:
+            self.MP_draw.draw_landmarks(img, l)
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import cv2
+from FaceTools import FaceDetectionTools
 
 capture = cv2.VideoCapture(0)
+FDT = FaceDetectionTools()
 
 while True:
     success, img = capture.read()
+    FDT.detect_face(img)
+    FDT.generate_mesh(img)
+    FDT.draw(img)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
