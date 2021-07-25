983e3d461f926de2328f4abdfc2419e080492cd9
Aydan Pirani <aydanpirani@gmail.com>
2021-07-25 14:54:34 -0700

    Broke up code to work for each patch separately

--- python/SkinDetection.py
+++ python/SkinDetection.py
@@ -4,6 +4,7 @@ import numpy as np
 from mediapipe.framework.formats.landmark_pb2 import NormalizedLandmarkList
 from matplotlib.path import Path
 from shapely.geometry import Polygon
+from sklearn.decomposition import NMF
 
 
 mpDraw = mp.solutions.drawing_utils
@@ -31,53 +32,65 @@ def get_points(n_img, landmarks):
 
         lcheek_area, rcheek_area = Polygon(lcheek_landmarks).area, Polygon(rcheek_landmarks).area
 
-        points = set() # Set of all pixels to change
+        forehead_pts = set() # Set of all pixels to change
+        rcheek_pts, lcheek_pts = set(), set()
 
         # Iterate through all pixels in image, check if pixel in path, then add
         for i in range(i_h):
             for j in range(i_w):
                 if forehead_path.contains_point((j, i)):
-                    points.add((i, j))
+                    forehead_pts.add((i, j))
 
                 # Check if the left cheek is at least 1/2 the size of the right cheek - if so, add to points
                 if lcheek_area/rcheek_area > 0.5 and lcheek_path.contains_point((j, i)):
-                    points.add((i, j))
+                    lcheek_pts.add((i, j))
 
                 # Same process ass mentioned above, but with right cheek
                 if rcheek_area/lcheek_area > 0.5 and rcheek_path.contains_point((j, i)):
-                    points.add((i, j))
+                    rcheek_pts.add((i, j))
 
     # Return all the points that are within 2 standard deviations of YUB values
-    return clean_data(n_img, points)
+
+    means, stds = get_stats(n_img, [forehead_pts, lcheek_pts, rcheek_pts])
+
+    return [clean_data(n_img, forehead_pts, means, stds),
+            clean_data(n_img, lcheek_pts, means, stds),
+            clean_data(n_img, rcheek_pts, means, stds)]
 
 
-# Given an image and a set of points, return only the points within 2 stds, for YUV values
-def clean_data(n_img, points):
+# Return the mean and stdevs for YUB values within the image, for select points
+def get_stats(n_img, arr):
     y, u, b = [], [], []
+    for points in arr:
+        for p0, p1 in points:
+            temp = n_img[p0, p1]
+            y.append(temp[0])
+            u.append(temp[1])
+            b.append(temp[2])
 
-    # Create lists of YUB values
-    for p0, p1 in points:
-        temp = n_img[p0, p1]
-        y.append(temp[0])
-        u.append(temp[1])
-        b.append(temp[2])
-
-    print("pre-cleaning", len(points))
-
-    # Get means and ranges for each variable
     y_mean, u_mean, b_mean = np.mean(y), np.mean(u), np.mean(b)
     y_std, u_std, b_std = np.std(y), np.std(u), np.std(b)
+
+    return (y_mean, u_mean, b_mean), (y_std, u_std, b_std)
+
+
+# Given an image, means/stdevs, and points, return only the points within 2 stds
+def clean_data(n_img, points, means, stds):
+    print("pre-cleaning", len(points))
+
+    y_mean, u_mean, b_mean = means
+    y_std, u_std, b_std = stds
+
     y_range, u_range, b_range = (y_mean - 2*y_std, y_mean + 2*y_std), (u_mean - 2*u_std, u_mean + 2*u_std), (b_mean - 2*b_std, b_mean + 2*b_std)
 
-    new_points = set()
+    new_points = []
     for p0, p1 in points:
         temp = n_img[p0, p1]
         # Check if point is within given ranges -> if so, add to new set
         if (y_range[0] <= temp[0] <= y_range[1]) and (u_range[0] <= temp[1] <= u_range[1]) and (b_range[0] <= temp[2] <= b_range[1]):
-            new_points.add((p0, p1))
+            new_points.append((p0, p1))
 
     print("post-cleaning", len(new_points))
-
     return new_points
 
 
@@ -102,7 +115,7 @@ def process_image(n_img, n_name):
 
     if results.multi_face_landmarks:
         points = get_points(imgYUB, results.multi_face_landmarks)
-        display_points(n_img, points, n_name)
+        # display_points(n_img, points, n_name)
 
     return imgYUB
 
