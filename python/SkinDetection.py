9a8a1b72617cba426186204981bb026f36897f8d
Aydan Pirani <aydanpirani@gmail.com>
2021-08-10 16:53:07 -0700

    Converted from YUV to RGB

--- python/SkinDetection.py
+++ python/SkinDetection.py
@@ -1,8 +1,6 @@
 import cv2
 import mediapipe as mp
-import numpy
 import numpy as np
-from mediapipe.framework.formats.landmark_pb2 import NormalizedLandmarkList
 from matplotlib.path import Path
 from shapely.geometry import Polygon
 from sklearn.decomposition import NMF, KernelPCA, PCA
@@ -51,7 +49,7 @@ def get_points(n_img, landmarks):
                 if rcheek_area/lcheek_area > 0.5 and rcheek_path.contains_point((j, i)):
                     rcheek_pts.add((i, j))
 
-    # Return all the points that are within 2 standard deviations of YUB values
+    # Return all the points that are within 2 standard deviations of RGB values
 
     means, stds = get_stats(n_img, [forehead_pts, lcheek_pts, rcheek_pts])
 
@@ -60,18 +58,19 @@ def get_points(n_img, landmarks):
             clean_data(n_img, rcheek_pts, means, stds)]
 
 
-# Return the mean and stdevs for YUB values within the image, for select points
+# Return the mean and stdevs for RGB values within the image, for select points
 def get_stats(n_img, arr):
-    y, u, b = [], [], []
+    r, g, b = [], [], []
+
     for points in arr:
         for p0, p1 in points:
             temp = n_img[p0, p1]
-            y.append(temp[0])
-            u.append(temp[1])
+            r.append(temp[0])
+            g.append(temp[1])
             b.append(temp[2])
 
-    means = (np.mean(y), np.mean(u), np.mean(b))
-    stds = (np.std(y), np.std(u), np.std(b))
+    means = (np.mean(r), np.mean(g), np.mean(b))
+    stds = (np.std(r), np.std(g), np.std(b))
 
     return means, stds
 
@@ -80,16 +79,16 @@ def get_stats(n_img, arr):
 def clean_data(n_img, points, means, stds):
     print("pre-cleaning", len(points))
 
-    y_mean, u_mean, v_mean = means
-    y_std, u_std, v_std = stds
+    r_mean, g_mean, b_mean = means
+    r_std, g_std, b_std = stds
 
-    y_range, u_range, v_range = (y_mean - 2*y_std, y_mean + 2*y_std), (u_mean - 2*u_std, u_mean + 2*u_std), (v_mean - 2*v_std, v_mean + 2*v_std)
+    r_range, g_range, b_range = (r_mean - 2*r_std, r_mean + 2*r_std), (g_mean - 2*g_std, g_mean + 2*g_std), (b_mean - 2*b_std, b_mean + 2*b_std)
 
     new_points = []
     for p0, p1 in points:
         temp = n_img[p0, p1]
         # Check if point is within given ranges -> if so, add to new set
-        if (y_range[0] <= temp[0] <= y_range[1]) and (u_range[0] <= temp[1] <= u_range[1]) and (v_range[0] <= temp[2] <= v_range[1]):
+        if (r_range[0] <= temp[0] <= r_range[1]) and (g_range[0] <= temp[1] <= g_range[1]) and (b_range[0] <= temp[2] <= b_range[1]):
             new_points.append((p0, p1))
 
     print("post-cleaning", len(new_points))
@@ -102,6 +101,7 @@ def display_points(n_img, points, n_name):
         for p in arr:
             s.add(p)
 
+    image = n_img.copy()
     invert = n_img.copy()
     i_h, i_w, i_c = n_img.shape
     for i in range(i_h):
@@ -109,24 +109,25 @@ def display_points(n_img, points, n_name):
             if (i, j) in s:
                 invert[i, j] = [0, 0, 0]
             else:
-                n_img[i, j] = [0, 0, 0]
+                image[i, j] = [0, 0, 0]
 
-    cv2.imwrite(f"../results/{n_name}_IMAGE.jpg", n_img)
+    cv2.imwrite(f"../results/{n_name}_IMAGE.jpg", image)
     cv2.imwrite(f"../results/{n_name}_INVERT.jpg", invert)
 
 
-def calculate_color(n_imgYUV, arr):
-    values = np.array([n_imgYUV[x, y] for x, y in arr]) / 256
-    print(values[:10]*256)
-    # print([256 * sum(v)/len(v) for v in values])
+def calculate_color(n_img, arr):
+
+    values = np.array([n_img[x, y] for x, y in arr]) / 256
     model = NMF(n_components=2, init='nndsvd', random_state=0, max_iter=500)
+
     W = model.fit_transform(values)
     H = model.components_
+
     print("W", W.shape)
     print("H", H.shape, H)
 
-    values = np.rot90(values)
-    print(np.array([256 * sum(v)/len(v) for v in values]))
+    # values = np.rot90(values)
+    # print(np.array([256 * sum(v)/len(v) for v in values]))
 
 
     # TODO: test zeroth element of each row of H vs. the whole rowsum
@@ -159,32 +160,24 @@ def calculate_color(n_imgYUV, arr):
 
 
 def process_image(n_img, n_name):
-    imgYUV = cv2.cvtColor(n_img, cv2.COLOR_BGR2YUV)
     imgRGB = cv2.cvtColor(n_img, cv2.COLOR_BGR2RGB)
-    # imgRGB = n_img
-    # imgYUV = cv2.cvtColor(n_img, cv2.COLOR_RGB2YUV)
 
     results = faceMesh.process(imgRGB)
 
     if results.multi_face_landmarks:
-        patches = get_points(imgYUV, results.multi_face_landmarks)
-        y_s, u_s, v_s = 0, 0, 0
+        patches = get_points(imgRGB, results.multi_face_landmarks)
+        r_s, g_s, b_s = 0, 0, 0
         for p in patches:
-            y, u, v = calculate_color(imgYUV, p)
-            y_s += y
-            u_s += u
-            v_s += v
-        print(y_s/3, u_s/3, v_s/3)
-        # display_points(n_img, points, n_name)
+            r, g, b = calculate_color(imgRGB, p)
+            r_s += r
+            g_s += g
+            b_s += b
+        print(r_s/3, g_s/3, b_s/3)
+        display_points(n_img, patches, n_name)
 
-    return imgYUV
+    return imgRGB
+
 
 name = "will_smith2"
 img = cv2.imread(f"../images/{name}.jpg", flags=cv2.IMREAD_COLOR)
-print(img.shape)
 img = process_image(img, name)
-# cv2.imwrite(f"../results/{name}_RGB.jpg", img)
-# imgYUV = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
-# imgYUV = cv2.cvtColor(imgYUV, cv2.COLOR_YUV2BGR)
-# cv2.imwrite(f"../results/{name}_YUV.jpg", imgYUV)
-#
