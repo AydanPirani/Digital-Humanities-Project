cbbd8e2ccd548028e33795368191d28209af2901
Aydan Pirani <aydanpirani@gmail.com>
2021-11-23 17:19:11 -0800

    Updated code

--- python/driver.py
+++ python/driver.py
@@ -4,9 +4,11 @@ import cv2
 # img = cv2.imread("../images/morgan_freeman.jpg")
 
 s = SkinDetector()
-# s.process(img, "morgan_freeman")
 
 from SkinDetector import SkinDetector
 s = SkinDetector()
-data = s.generate_json("morgan_freeman", "../images/morgan_freeman.jpg")
-print(data)
+# s.process(img, "morgan_freeman")
+
+# data = s.generate_json("morgan_freeman", "../images/morgan_freeman.jpg")
+# print(data)
+s.process("testing",  "../images/ariana_grande.jpg", {"display_points":True})
